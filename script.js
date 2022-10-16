0ed2943c016b112f478a36c4b89fbb88d7f61ae5
John Gibson <jhngbsn09@gmail.com>
2022-10-16 16:41:33 -0400

    Created constructor for books

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+class book {
+    constructor(read, author, title, pages) {
+        this.readStatus = read;
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+    }
+}
+
+function addBookToLibrary(){
+
+}
